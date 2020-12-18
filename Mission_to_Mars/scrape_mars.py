d82a7ddb346b7b941c3ade4aa97f0d902f309a60
will <will.gaechter@gmail.com>
2020-12-18 16:53:34 -0500

    updating with compelted news scrape

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,6 +1,7 @@
 from splinter import Browser
 from bs4 import BeautifulSoup
 import pandas as pd
+import pymongo
 
 def init_browser():
     executable_path = {"executable_path": "C:\Program Files\Google\Chrome\Application\chromedriver.exe"}
@@ -28,9 +29,14 @@ def IMGscrape():
     return featured_image_url
 
 hemisphere_image_urls_test = []
+news_list = []
 
 def scrape():
+    ### CALL IN SPLINTER ###
+
     browser = init_browser()
+
+    ### INITIAL IMAGE PULL
 
     img_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(img_url)
@@ -46,7 +52,23 @@ def scrape():
 
     featured_image_url = (str(base_url) + str(img_url_short))
 
-    return featured_image_url
+    ### NEWS SCRAPE ###
+
+    news_url = 'https://mars.nasa.gov/news/'
+    browser.visit(news_url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+
+    results = soup.find_all('div', class_='list_text')
+
+    for r in results:
+        news_title = r.find('a').text
+        IP = r.find('div', class_="article_teaser_body").text
+
+        news_dict = {"title": news_title, "body": IP}
+        news_copy = news_dict.copy()
+        news_list.append(news_copy)
 
     ### PANDAS FACTS TABLE
 
@@ -58,8 +80,8 @@ def scrape():
     ### HEMISPHERE IMAGE PULL
     url_list = ['https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced', 'https://astrogeology.usgs.gov/search/map/Mars/Viking/schiaparelli_enhanced', 'https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced', 'https://astrogeology.usgs.gov/search/map/Mars/Viking/valles_marineris_enhanced']
 
-    for url in url_list:
-        browser.visit(url)
+    for u in url_list:
+        browser.visit(u)
 
         html = browser.html
         soup = BeautifulSoup(html, "html.parser")
@@ -76,7 +98,23 @@ def scrape():
         hemi_copy = hemi_dict.copy()
         hemisphere_image_urls_test.append(hemi_copy)
 
+    return featured_image_url
+    return mars_facts.head(1)
     return print(hemisphere_image_urls_test)
+    return print(news_list)
+
+    ### UPLOAD TO LOCAL MONGO DB ###
+
+    conn = 'mongodb://localhost:27017'
+    client = pymongo.MongoClient(conn)
+
+    db = client.hemisphere_db
+    collection = db.items
+
+    collection.insert_
+
+### FLASK SERVER ###
+
 
 
 
